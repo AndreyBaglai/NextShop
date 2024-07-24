08b5c26d3c0b77ec9d09fdf349172863dd5e8974
Andrey Baglay <andreybaglai@gmail.com>
2024-07-24 16:36:41 +0300

    add range slider for filters

--- src/components/shared/filters.tsx
+++ src/components/shared/filters.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import React from "react";
-import { FilterCheckbox, Title } from ".";
+import { FilterCheckbox, RangeSlider, Title } from ".";
+import { Input } from "../ui";
 
 interface FiltersProps {
   className?: string;
@@ -14,6 +15,22 @@ export const Filters: React.FC<FiltersProps> = ({ className }) => {
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="A" value="1" />
         <FilterCheckbox text="News" value="2" />
+      </div>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Price from and to:</p>
+        <div className="flex gap-3 mb-5">
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            defaultValue={0}
+          />
+          <Input type="number" placeholder="1000" min={100} max={1000} />
+        </div>
+
+        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
       </div>
     </div>
   );
