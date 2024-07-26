307148e2eeef37b6bdb2e74c62dcb33f5fb5f5a7
Andrey Baglay <andreybaglai@gmail.com>
2024-07-26 16:39:53 +0300

    Add input search layout in the header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -2,9 +2,10 @@ import { cn } from "@/lib/utils";
 import React from "react";
 
 import Image from "next/image";
-import { Container } from ".";
+import { Container, SearchInput } from ".";
 import { Button } from "../ui";
 import { ArrowRight, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
 
 interface HeaderProps {
   className?: string;
@@ -14,12 +15,18 @@ export const Header: React.FC<HeaderProps> = ({ className }) => {
   return (
     <header className={cn("border border-b", className)}>
       <Container className="flex items-center justify-between py-8">
-        <div className="flex items-center gap-4">
-          <Image src="/logo.png" width={35} height={35} alt="Logo" />
-          <div>
-            <h1 className="text-2xl uppercase font-black">Home title</h1>
-            <p className="text-sm text-gray-400 leading-3">Sub text title</p>
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <Image src="/logo.png" width={35} height={35} alt="Logo" />
+            <div>
+              <h1 className="text-2xl uppercase font-black">Home title</h1>
+              <p className="text-sm text-gray-400 leading-3">Sub text title</p>
+            </div>
           </div>
+        </Link>
+
+        <div className="mx-10 flex-1">
+          <SearchInput />
         </div>
 
         <div className="flex items-center gap-3">
