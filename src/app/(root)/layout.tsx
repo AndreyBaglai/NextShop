30ce469fae950b8d9f76f5448465faf70da87457
Andrey Baglay <andreybaglai@gmail.com>
2024-07-31 15:57:07 +0300

    Refactor page layouts

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "../globals.css";
 import { Header } from "@/components/shared";
-
-const nunito = Nunito({
-  subsets: ["cyrillic"],
-  variable: "--font-nunito",
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
 
 export const metadata: Metadata = {
   title: "Next Shop | Home",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
@@ -19,13 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={nunito.className}>
-        <main className="min-h-screen">
-          <Header />
-          {children}
-        </main>
-      </body>
-    </html>
+    <main className="min-h-screen">
+      <Header />
+      {children}
+    </main>
   );
 }
