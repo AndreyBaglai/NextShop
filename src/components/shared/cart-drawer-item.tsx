48a8fb42c119f1a0a8dcce577da691b13af531a0
Andrey Baglay <andreybaglai@gmail.com>
2024-08-05 17:34:39 +0300

    Add layout for cart item drawer

---
+++ src/components/shared/cart-drawer-item.tsx
@@ -0,0 +1,12 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface CartDrawerItemProps {
+  className?: string;
+}
+
+export const CartDrawerItem: React.FC<CartDrawerItemProps> = ({
+  className,
+}) => {
+  return <div></div>;
+};
